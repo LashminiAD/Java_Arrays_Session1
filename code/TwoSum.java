ceac11b38951d3071c4b62e3743faa8433024e8f
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-03-10 21:20:56 +0530

    Implement brute force solution for Two Sum problem
    Added a brute force implementation of the Two Sum problem for educational purposes.

--- code/TwoSum.java
+++ code/TwoSum.java
@@ -1,13 +1,30 @@
 import java.util.HashMap;
 
+class TwoSumBruteForce {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
+
+/*
+In the first session, I intentionally explained the Brute Force approach because it helps beginners understand the core logic of checking pairs.
+Once the concept becomes clear, we move to the optimized HashMap approach, which improves the time complexity from O(n²) to O(n).
+*/
+
 public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
-
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-
             int complement = target - nums[i];
 
             if (map.containsKey(complement)) {
