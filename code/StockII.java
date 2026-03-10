cd7d4fdcc5c2e75070032ff1b027b0d5af8facf4
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-03-10 21:16:54 +0530

    Implement maxProfit method in StockII class

--- code/StockII.java
+++ code/StockII.java
@@ -1,1 +1,16 @@
+public class StockII {
 
+    public static int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
