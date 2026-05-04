c854ff5afd72e3cb05e03820bf59936c20916137
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-05-04 22:17:51 +0530

    Replace selection sort with bubble sort implementation

--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -1,16 +1,23 @@
 class Solution {
-    void selectionSort(int[] arr) {
+    public void bubbleSort(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i<n; i++){
-            int min = i;
-            for(int j = i+1; j<n; j++){
-                if(arr[j]<arr[min]){
-                    min = j;
+        for(int i = 0; i<n; i++){  // or (int i = n - 1; i >= 0; i--)
+            
+                boolean swapped = false; //if no swap occurs in iteration i = 0 then all are sorted no need to checck the rest i = 1 to n
+
+            for(int j = 0; j<n-i-1; j++){  // or (int j = 0; j <= i - 1; j++) 
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    
+                    swapped = true;
                 }
             }
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
+            
+            if(!swapped){
+                break;
+            }
         }
     }
 }
