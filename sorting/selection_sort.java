b84324ee61dbcdfdab20000a23ffb3503ced98a2
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-05-04 22:19:49 +0530

    Implement selection sort algorithm

--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -1,23 +1,17 @@
 class Solution {
-    public void bubbleSort(int[] arr) {
+    void selectionSort(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i<n; i++){  // or (int i = n - 1; i >= 0; i--)
+        for(int i = 0; i<n; i++){
+            int min = i;
             
-                boolean swapped = false; //if no swap occurs in iteration i = 0 then all are sorted no need to checck the rest i = 1 to n
-
-            for(int j = 0; j<n-i-1; j++){  // or (int j = 0; j <= i - 1; j++) 
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    
-                    swapped = true;
+            for(int j = i+1; j<n; j++){
+                if(arr[j]<arr[min]){
+                    min = j;
                 }
             }
-            
-            if(!swapped){
-                break;
-            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
     }
 }
